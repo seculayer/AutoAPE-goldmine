69abda7b8daeef6464650ee3f5da03b57c3083a0
Jin Kim <jinkim@seculayer.co.kr>
2020-05-21 18:17:46 +0900

    TensorFlowAbstract

--- experiment/hyper-params-search/hps/ml/TensorFlowAbstract.py
+++ experiment/hyper-params-search/hps/ml/TensorFlowAbstract.py
@@ -4,3 +4,12 @@
 # Powered by Seculayer © 2020 Solution Development 2 Team, R&D Center. 
 
 # class : class_name
+class TensorFlowAbstract(object):
+    def __init__(self, param_dict):
+        pass
+
+    def _check_parameter(self, param_dict):
+        raise NotImplementedError
+
+    def _build(self):
+        raise NotImplementedError
