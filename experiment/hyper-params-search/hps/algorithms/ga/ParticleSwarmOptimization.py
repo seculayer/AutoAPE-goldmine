4a87fe54eb218c47229f0ee7e3a176da3eab24d2
GyunHyukLee <gyunhyuk.lee@seculayer.com>
2021-11-03 18:52:03 +0900

    Add HPO algorithm : Particle Swarm Optimization

--- experiment/hyper-params-search/hps/algorithms/ga/ParticleSwarmOptimization.py
+++ experiment/hyper-params-search/hps/algorithms/ga/ParticleSwarmOptimization.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from hps.algorithms.HPOptimizationAbstract import HPOptimizationAbstract
+from hps.algorithms.HPOptimizerUtil import HPOptimizerUtil
 
 
 class ParticleSwarmOptimization(HPOptimizationAbstract):
@@ -18,8 +19,7 @@ class ParticleSwarmOptimization(HPOptimizationAbstract):
         self._delta = self._hpo_params["delta"]    # modified PSO
 
     # generate candidate function
-    def _generate(self, param_list, score_list, iter_num):
-
+    def _generate(self, param_list, score_list):
         result_param_list = list()
         p_best_list = list()
 
@@ -36,8 +36,11 @@ class ParticleSwarmOptimization(HPOptimizationAbstract):
 
 
         # if duplicate, generate new particle
-        result_param_list = self._remove_duplicate_params(result_param_list)
+        result_param_list = HPOptimizerUtil.remove_duplicated_params(self.unique_param_dict, update_position_params)
         num_result_params = len(result_param_list)
+        for result_param in result_param_list:
+            temp_hash = HPOptimizerUtil.param_dict_to_hash(result_param)
+            self.unique_param_dict[temp_hash] = {"param_dict": result_param}
         ## leak
         if num_result_params < self._n_pop:
             result_param_list += self._generate_param_dict_list(self._n_pop - num_result_params)
@@ -63,6 +66,7 @@ class ParticleSwarmOptimization(HPOptimizationAbstract):
                 if max_score_value == score_list[i]:
                     return param_list[i]
 
+
     # global에서 최대
     def _g_best(self, param_list, p_best_list):
         if len(p_best_list) == 0:
@@ -82,7 +86,57 @@ class ParticleSwarmOptimization(HPOptimizationAbstract):
             return param_list
 
 
+    def compute_velocity(self,param_dict_list, pos_best_i, g_best_i):
+        # param_dict_list, p_best_value, g_best_value
+        #w = random.uniform(0,1)
+        # self.LOGGER.info("input / param_dict_list length, pos_best_i, g_best_i : {}, {}, {} ".format(len(param_dict_list), pos_best_i, g_best_i))
+        w = 0.75
 
+        # initialize each velocity dictionary in list
+        velocity_list = list()
+        velocity_dict = dict()
+        for _, key in enumerate(self._pbounds):
+            velocity_dict[key] = random.uniform(-1, 1)
+
+        for _ in range(len(param_dict_list)):
+            velocity_list.append(velocity_dict)
+
+        for i, param_dict in enumerate(param_dict_list):
+            for j in param_dict.keys():
+                r1 = random.random()
+                r2 = random.random()
+
+                # modified velocity for multi-dim
+                # self.LOGGER.info("velocity_list: :{}".format(velocity_list))
+                if type(param_dict[j]) == int or type(param_dict[j]) == float:
+                    vel_cognitive = self._c1*r1*(pos_best_i[j] - param_dict[j])
+                    vel_social = self._c2*r2*(g_best_i[j] - param_dict[j])
+                    # self.LOGGER.info("i, j, velocity_list :{},{}, {}".format(i, j, velocity_list[i][j]))
+                    velocity_list[i][j] = w * velocity_list[i][j] + vel_cognitive + vel_social
+
+                else :
+                    vel_cognitive = self._c1 * r1
+                    vel_social = self._c2 * r2
+                    velocity_list[i][j] = w * velocity_list[i][j] + vel_cognitive + vel_social
+
+        return velocity_list
+
+    # update position based on updated velocity
+    def update_position(self, param_list, velocity_i):
+
+        for i, param_dict in enumerate(param_list):
+            for j in param_dict.keys():
+                if type(param_dict[j]) == int or type(param_dict[j]) == float:
+                    param_dict[j] = param_dict[j] + velocity_i[i][j]
+                    # 범위 설정
+                    min = self._pbounds[j][0]
+                    max = self._pbounds[j][1]
+                    param_dict[j] = np.clip(param_dict[j], min, max)
+                # categorical 변수의 경우
+                else :
+                    param_dict[j] = param_dict[j]
+        return param_list
+    '''
     def compute_velocity(self,param_dict_list, pos_best_i, g_best_i):
         # random inertia weight
         #w = random.uniform(0,1)
@@ -113,23 +167,7 @@ class ParticleSwarmOptimization(HPOptimizationAbstract):
                     velocity_list[i][j] = w * velocity_list[i][j] + vel_cognitive + vel_social
 
         return velocity_list
-
-
-    # update position based on updated velocity
-    def update_position(self, param_list, velocity_i):
-
-        for i, param_dict in enumerate(param_list):
-            for j in param_dict.keys():
-                if type(param_dict[j]) == int or type(param_dict[j]) == float:
-                    param_dict[j] = param_dict[j] + velocity_i[i][j]
-                    # 범위 설정
-                    min = self._pbounds[j][0]
-                    max = self._pbounds[j][1]
-                    param_dict[j] = np.clip(param_dict[j], min, max)
-                # categorical 변수의 경우
-                else :
-                    param_dict[j] = param_dict[j]
-        return param_list
+    '''
 
 
 # main __init__ to execute in this single file
